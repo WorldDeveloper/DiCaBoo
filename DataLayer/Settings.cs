e4a5e1742c5357bdb18fb8ef7040c779c4973efc
WorldDeveloper <matviienkos@gmail.com>
2015-09-11 01:59:21 +0300

    Created Settings.cs

--- DataLayer/Settings.cs
+++ DataLayer/Settings.cs
@@ -1,17 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    class Settings
+    public class Settings
     {
-        public int ID { get; set; }
-        public string FacebookAppToken { get; set; }
-        public string FacebookUserId { get; set; }
+        public int ID { get; }
+        public string FacebookAppToken { get; }
+        public string FacebookUserId { get; }
+
+        public Settings()
+        {
+            using (SqlConnection connection = DB.SqlConnection)
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "Select * from Settings;";
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            ID = (int)reader["Id"];
+                            FacebookAppToken = reader["FacebookAppToken"] as String;
+                            FacebookUserId=reader["FacebookUserId"] as String;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Settings(string facebookAppToken, string facebookUserId)
+        {
+            using (SqlConnection connection = DB.SqlConnection)
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE Settings SET FacebookAppToken=@fbAppToken, FacebookUserId=@fbUserId WHERE id=1;";
+
+                    SqlParameter fbAppToken = new SqlParameter("@fbAppToken", SqlDbType.NVarChar, -1);
+                    fbAppToken.Value = facebookAppToken;
+                    command.Parameters.Add(fbAppToken);
+
+                    SqlParameter fbUserId = new SqlParameter("@fbUserId", SqlDbType.NVarChar, -1);
+                    fbUserId.Value = facebookUserId;
+                    command.Parameters.Add(fbUserId);
+
+                    connection.Open();
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        ID = 1;
+                        FacebookAppToken = facebookAppToken;
+                        FacebookUserId = facebookUserId;
+                    }
 
 
+                }
+            }
+        }
     }
 }
