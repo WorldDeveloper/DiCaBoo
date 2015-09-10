1aaac83ea6d21fc7516e25cc9a452b2b99d0b217
WorldDeveloper <matviienkos@gmail.com>
2015-09-10 21:11:09 +0300

    Created table Settings.sql

---
+++ DataLayer/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Settings
+    {
+        public int ID { get; set; }
+        public string FacebookAppToken { get; set; }
+        public string FacebookUserId { get; set; }
+
+
+    }
+}
---
+++ dbDCB/tables/Settings.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Settings]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[FacebookAppToken] NVARCHAR(MAX) NULL,
+	[FacebookUserId] NVARCHAR(MAX) NULL
+);
+
+
+
