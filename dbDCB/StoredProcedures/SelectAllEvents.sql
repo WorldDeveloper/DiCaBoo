2d75988fed3074c6abb2fec8d33189605bd5d0d5
WorldDeveloper <matviienkos@gmail.com>
2015-09-11 14:31:01 +0300

    Created stored procedures SelectAllEvents and SelectEventsByType

---
+++ dbDCB/StoredProcedures/SelectAllEvents.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[SelectAllEvents]
+	@from DateTime2(0),
+	@to DateTime2(0)
+AS
+	SELECT * FROM CalendarView WHERE EventDateTime BETWEEN @from AND @to
+
---
+++ dbDCB/StoredProcedures/SelectEventsByType.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[SelectEventsByType]
+	@from DateTime2(0),
+	@to DateTime2(0),
+	@eventTypeId int
+AS
+	SELECT * FROM CalendarView WHERE EventDateTime BETWEEN @from AND @to AND EventTypeId=@eventTypeId
