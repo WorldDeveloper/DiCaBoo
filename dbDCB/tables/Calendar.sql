69a161730228690c1d423b9476bdf42cc2f62596
WorldDeveloper <matviienkos@gmail.com>
2015-09-11 14:28:46 +0300

    Created Calendar.sql

---
+++ dbDCB/tables/Calendar.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Calendar]
+(
+	[EventId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[EventTypeId] INT NOT NULL,
+	[EventDateTime] DATETIME2(0) NOT NULL,
+	[EventTitle] NVARCHAR(250) NOT NULL,
+	[EventDescription] NVARCHAR(MAX) NULL,
+	[EventVenue] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_Calendar_ToEventTypes] FOREIGN KEY (EventTypeId) REFERENCES EventTypes(Id)
+)
