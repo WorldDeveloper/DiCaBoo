6a46a1c3407ddc0aaf3fe2aa94923bdae43c265c
WorldDeveloper <matviienkos@gmail.com>
2015-09-25 16:01:52 +0300

    Added OperationsView.sql

---
+++ dbDCB/Views/OperationsView.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[OperationsView]
+	AS SELECT CreditId, Credit.AccountName AS CreditName, DebitId, Debit.AccountName AS DebitName, Summ, Note  
+	FROM Operations 
+	JOIN Accounts AS Credit ON Operations.CreditId=Credit.AccountId 
+	JOIN Accounts AS Debit ON Operations.DebitId=Debit.AccountId
