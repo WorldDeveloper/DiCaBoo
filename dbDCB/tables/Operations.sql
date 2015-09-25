4acf085cf415e6c05434eb19d1bec9b9e2ff20e4
WorldDeveloper <matviienkos@gmail.com>
2015-09-25 15:49:45 +0300

    Added table Operations.sql

---
+++ dbDCB/tables/Operations.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Operations]
+(
+	[OperationId] INT NOT NULL PRIMARY KEY,
+	Credit hierarchyid NOT NULL REFERENCES Accounts(AccountId),
+	Debit hierarchyid NOT NULL REFERENCES Accounts(AccountId),
+	Summ money not null CHECK (Summ>0),
+	Note nvarchar(200)
+)
