1615cb07fcc6521fcac0353cbe184989616daca4
WorldDeveloper <matviienkos@gmail.com>
2015-09-25 15:53:36 +0300

    Added AddAccount store procedure

---
+++ dbDCB/StoredProcedures/AddAccount.sql
@@ -0,0 +1,13 @@
+﻿CREATE procedure [dbo].AddAccount(@parentId hierarchyid, @accountName nvarchar(100))
+AS
+BEGIN
+DECLARE @lastChild hierarchyid
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+BEGIN TRANSACTION
+
+UPDATE Accounts 
+SET @lastChild=LastChild=AccountId.GetDescendant(LastChild, NULL)
+WHERE AccountId=@parentId
+INSERT Accounts(AccountId, AccountName) VALUES(@lastChild, @accountName)
+COMMIT
+END;
