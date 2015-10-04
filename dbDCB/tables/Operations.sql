96b1725063efd3c12baae627db53532007e8b18f
WorldDeveloper <matviienkos@gmail.com>
2015-10-04 10:48:36 +0300

    Added OperationDate to Operations.sql table

--- dbDCB/tables/Operations.sql
+++ dbDCB/tables/Operations.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Operations]
 (
 	[OperationId] INT NOT NULL PRIMARY KEY,
+	[OperationDate] DATE NOT NULL DEFAULT GETDATE(),
 	[CreditId] hierarchyid NOT NULL REFERENCES Accounts(AccountId),
 	[DebitId] hierarchyid NOT NULL REFERENCES Accounts(AccountId),
-	Summ money not null CHECK (Summ>0),
-	Note nvarchar(200)
+	[Summ] money not null CHECK (Summ>0),
+	[Note] nvarchar(200) 
+
 )
