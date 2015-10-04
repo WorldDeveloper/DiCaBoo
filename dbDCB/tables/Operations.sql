7386a70e259791e7d47a67c93544189d131ff4c4
WorldDeveloper <matviienkos@gmail.com>
2015-10-04 20:12:07 +0300

    Modified Operations.sql: added identity, removed check sum constraint

--- dbDCB/tables/Operations.sql
+++ dbDCB/tables/Operations.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Operations]
 (
-	[OperationId] INT NOT NULL PRIMARY KEY,
+	[OperationId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[OperationDate] DATE NOT NULL DEFAULT GETDATE(),
 	[CreditId] hierarchyid NOT NULL REFERENCES Accounts(AccountId),
 	[DebitId] hierarchyid NOT NULL REFERENCES Accounts(AccountId),
-	[Summ] money not null CHECK (Summ>0),
+	[Summ] money not null,
 	[Note] nvarchar(200) 
 
 )
